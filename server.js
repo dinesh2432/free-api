286702e4b31b7e8e81e1651b4eae8fb5c2755819
dinesh2432 <ddk113311@gmail.com>
2026-01-05 09:31:39 +0530

    added one

--- server.js
+++ server.js
@@ -96,3 +96,7 @@ app.use(cors())
 app.get("/", (req, res)=>{
     res.json(data);
 })
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
